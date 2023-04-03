4306c32fa0f098daa44b40c5f3b4c4299bbf874b
spirus-dev <43066360+spirus-dev@users.noreply.github.com>
2023-04-03 22:10:32 +0530

    Form functionality to add books is added
    Now book is added to the bookShelf using form functionality. Further we need to add controls in each of the book card. And after completing that we'll also improve styling to make it look cooler.

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function showLibrary(){
             console.log(`${book.title} by ${book.author}, ${book.pages} pages, ${book.isRead?'read':'not read yet'}`);
             const displayBook= document.createElement('div');
             displayBook.innerText=`${book.title} by ${book.author}, ${book.pages} pages, ${book.isRead?'read':'not read yet'}`;
+            displayBook.classList.add('card')
             bookShelf.appendChild(displayBook);
         });
     }
@@ -46,5 +47,25 @@ function showForm(e){
 
 const addButton = document.querySelector('#add');
 addButton.addEventListener('click',showForm);
+
+const addBook = document.querySelector('#addBook');
+addBook.addEventListener('click', function(event){
+    event.preventDefault();
+    const bookTitle=document.querySelector('#bookTitle');
+    const bookAuthor=document.querySelector('#bookAuthor');
+    const bookPages=document.querySelector('#bookPages');
+    const bookRead=document.querySelector('#bookRead');
+    if(bookTitle.value===''||bookAuthor.value===''||bookPages.value===''){
+        alert('Please fill the necessary details');
+    }
+    else{
+        addBookToLibrary(bookTitle.value,bookAuthor.value,bookPages.value,bookRead.checked);
+        bookTitle.value='';
+        bookAuthor.value='';
+        bookPages.value='';
+        bookRead.checked=false;
+        showForm();
+    }
+});
 
 showLibrary();
