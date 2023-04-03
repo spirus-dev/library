4f52c477f380ae58f0c4a4948c1bc9f75832b58e
spirus-dev <43066360+spirus-dev@users.noreply.github.com>
2023-04-04 00:38:34 +0530

    Library is ready with all it's functionality
    All the functional requirements completed. Our Library can add, update and remove books and can display the appropriate message when the Library is empty. Needs to improve styling in future.

--- script.js
+++ script.js
@@ -21,15 +21,51 @@ function showLibrary(){
             child = bookShelf.lastElementChild;
         }
     if(myLibrary.length==0){
-        console.log('No books in the library');
+        const noBook=document.createElement('div');
+        noBook.className='noBook';
+        noBook.innerText='No Books Available';
+        bookShelf.appendChild(noBook);
     }
     else{
+        let index=0;
         myLibrary.forEach(book => {
-            console.log(`${book.title} by ${book.author}, ${book.pages} pages, ${book.isRead?'read':'not read yet'}`);
             const displayBook= document.createElement('div');
-            displayBook.innerText=`${book.title} by ${book.author}, ${book.pages} pages, ${book.isRead?'read':'not read yet'}`;
+
+            const bookTitle=document.createElement('div')
+            bookTitle.classList.add('bookTitle');
+            bookTitle.innerHTML=`<span class="cardHeader">Book Title</span> : ${book.title}`;
+
+            const bookAuthor=document.createElement('div')
+            bookAuthor.classList.add('bookAuthor');
+            bookAuthor.innerHTML=`<span class="cardHeader">Book Author</span> : ${book.author}`;
+
+            const bookPages=document.createElement('div')
+            bookPages.classList.add('bookPages');
+            bookPages.innerHTML=`<span class="cardHeader">Book Pages</span> : ${book.pages}`;
+
+            const bookRead=document.createElement('div')
+            bookRead.classList.add('bookRead');
+            bookRead.innerHTML=`<span class="cardHeader">Finished Reading?</span> : ${book.isRead}`;
+
+            const readButton=document.createElement('button');
+            readButton.classList.add('readButton');
+            readButton.dataset.index=`${index}`;
+            readButton.innerText='Read';
+
+            const deleteButton=document.createElement('button');
+            deleteButton.classList.add('deleteButton');
+            deleteButton.innerText='Delete';
+            deleteButton.dataset.index=`${index}`;
+
+            displayBook.appendChild(bookTitle);
+            displayBook.appendChild(bookAuthor);
+            displayBook.appendChild(bookPages);
+            displayBook.appendChild(bookRead);
+            displayBook.appendChild(readButton);
+            displayBook.appendChild(deleteButton);
             displayBook.classList.add('card')
             bookShelf.appendChild(displayBook);
+            index++;
         });
     }
 }
@@ -65,6 +101,29 @@ addBook.addEventListener('click', function(event){
         bookPages.value='';
         bookRead.checked=false;
         showForm();
+    }
+});
+
+const bookShelf=document.querySelector('.bookShelf');
+bookShelf.addEventListener('click',function(event){
+    let targetElement= event.target;
+    if(targetElement.className==="deleteButton"){
+        let index=Number(targetElement.dataset.index);
+        let removedTitle=myLibrary[index].title;
+        let removed=myLibrary.splice(index,1);
+        alert(`Book named "${removedTitle}" is removed from the library`);
+        showLibrary();
+    }
+    else if(targetElement.className==="readButton"){
+        let index=Number(targetElement.dataset.index);
+        let readStatus=myLibrary[index].isRead;
+        if(readStatus===true){
+            myLibrary[index].isRead=false;
+        }
+        else{
+            myLibrary[index].isRead=true;
+        }
+        showLibrary()
     }
 });
 
