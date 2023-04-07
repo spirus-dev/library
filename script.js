9789c3d0332dd9bfc5061b6eba60d96fc7b1feec
spirus-dev <43066360+spirus-dev@users.noreply.github.com>
2023-04-07 16:35:01 +0530

    Constructor Function Pattern to Class Pattern

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let myLibrary = [];
 
-function Book(title,author,pages,isRead) {
-  this.title=title;
-  this.author=author;
-  this.pages=pages;
-  this.isRead=isRead;
+class Book{
+    constructor(title,author,pages,isRead) {
+        this.title=title;
+        this.author=author;
+        this.pages=pages;
+        this.isRead=isRead;
+      }
 }
 
 function addBookToLibrary(title,author,pages,isRead) {
